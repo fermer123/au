d7c35976f12a9141ce7e34a5f6e0690349dd288d
fermer123 <fermer_525@mail.ru>
2022-04-25 22:36:03 +0500

    select

--- src/components/location/location.jsx
+++ src/components/location/location.jsx
@@ -1,6 +1,24 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import style from './location.module.scss';
 
 const Location = () => {
+  const [unversity, setUnversity] = useState([]);
+  const [value, setValue] = useState('');
+  useEffect(() => {
+    const fetchUniversity = async () => {
+      const response = await axios(
+        'http://universities.hipolabs.com/search?country=United+Kingdom',
+      );
+      setUnversity(response.data);
+    };
+    fetchUniversity();
+  }, []);
+
+  const option = unversity.map((e, index) => {
+    return <option key={index}>{e.name}</option>;
+  });
+
   return (
     <div className={style.location}>
       <div className={style.location_city}>
@@ -9,7 +27,13 @@ const Location = () => {
       </div>
       <div className={style.location_university}>
         <div className={style.university}>Ваш университет</div>
-        <select></select>
+        <select
+          className={style.university_select}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        >
+          {option}
+        </select>
       </div>
     </div>
   );
