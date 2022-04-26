e069baf79f14f60a324e2eea8cc08e71fa8f8236
fermer123 <fermer_525@mail.ru>
2022-04-26 20:27:52 +0500

    pass

--- src/components/password/password.jsx
+++ src/components/password/password.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import style from './password.module.scss';
 
 const Password = () => {
+  const [pass, setPass] = useState('');
+  const [repeatPass, setRepeatPass] = useState('');
   return (
     <div className={style.password}>
-      <div className={style.password_first}>
-        <div className={style.password_once}>Пароль</div>
+      <div className={style.password_wrapper}>
+        <div>Пароль</div>
+        <input
+          type='password'
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+        />
       </div>
-      <div className={style.password_second}>
-        <div className={style.password_twice}>Пароль еще раз</div>
+      <div className={style.password_wrapper}>
+        <div>Пароль еще раз</div>
+        <input
+          type='password'
+          value={repeatPass}
+          onChange={(e) => setRepeatPass(e.target.value)}
+        />
       </div>
     </div>
   );
