0532e9845fc6bf310cf219c4066244b90abee305
fermer123 <fermer_525@mail.ru>
2022-04-28 00:37:52 +0500

    emailvalienotwork

--- src/components/email/email.jsx
+++ src/components/email/email.jsx
@@ -4,18 +4,45 @@ import style from './email.module.scss';
 const Email = () => {
   const [checked, setChecked] = useState(true);
   const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState(false);
+  const [emailError, setEmailError] = useState('Неверный E-mail');
+  const [emailDirty, setEmailDirty] = useState(false);
 
   const checkbox = () => {
     setChecked(!checked);
+  };
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        setEmailDirty(true);
+        break;
+    }
+  };
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(e.target.value.toLowerCase()))) {
+      setEmailError('Неверный E-mail');
+    } else {
+      setEmailError('');
+    }
   };
 
   return (
     <div className={style.wrapper}>
       <div className={style.email_wrapper}>
         <div className={style.password_field}>Электронная почта</div>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} />
-        {emailError && <div className={style.email_error}>неверный E-mail</div>}
+        <input
+          name='email'
+          onBlur={(e) => blurHandler()}
+          value={email}
+          onChange={(e) => emailHandler(e)}
+        />
+        {emailError && emailDirty && (
+          <div className={style.email_error}>неверный E-mail</div>
+        )}
       </div>
       <div className={style.password_wrapper}>
         <div className={style.password_agree}>Я согласен</div>
