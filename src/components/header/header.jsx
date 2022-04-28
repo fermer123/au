f8b2893c75dd008743a89301d70e4fd2f4b0ec9b
fermer123 <fermer_525@mail.ru>
2022-04-28 21:58:16 +0500

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,38 @@
+import { useState } from 'react';
 import style from './header.module.scss';
+
 const Header = () => {
+  const [status, setStatus] = useState(false);
+  const [change, setChange] = useState('');
+
+  const handlerIn = (e) => {
+    setChange(e.target.value);
+  };
+
+  const handlerBtn = () => {
+    if (!status) {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
+  };
+
   return (
     <div className={style.header}>
       <div className={style.header_person}>Здравствуйте, Человек №3596941</div>
-      <button className={style.header_change_status}>Сменить статус</button>
+      <button onClick={handlerBtn} className={style.header_change_status}>
+        Сменить статус
+      </button>
+
+      {status ? (
+        <div className={style.header_change_field}>
+          <input placeholder={change} onChange={handlerIn} />
+        </div>
+      ) : change ? (
+        <div className={style.header_change_field}>{change}</div>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
